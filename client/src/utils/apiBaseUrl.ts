dac3330567f202aa4e7cf542392bea1bdbbe67a2
unknown <hcmiki358@gmail.com>
2023-08-18 14:59:55 +0200

    added redirects and updated api url

--- client/src/utils/apiBaseUrl.ts
+++ client/src/utils/apiBaseUrl.ts
@@ -1,3 +1,6 @@
-const baseUrl = "https://social-pern.onrender.com";
+const baseUrl =
+  import.meta.env.NODE_ENV === "production"
+    ? import.meta.env.API_BASE_URL
+    : "http://localhost:3001";
 
 export default baseUrl;
