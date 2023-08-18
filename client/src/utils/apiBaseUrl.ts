8bc5a34b002ca9ec57948ba78a63f999baed5f85
unknown <hcmiki358@gmail.com>
2023-08-18 15:46:02 +0200

    changed api url

--- client/src/utils/apiBaseUrl.ts
+++ client/src/utils/apiBaseUrl.ts
@@ -1,6 +1,3 @@
-const baseUrl =
-  import.meta.env.NODE_ENV === "development"
-    ? import.meta.env.API_BASE_URL
-    : "http://localhost:3001";
+const baseUrl = import.meta.env.API_BASE_URL;
 
 export default baseUrl;
