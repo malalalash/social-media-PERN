28a17ee0b5053abee7c2af4d54cbfb8247ba84d7
Mikołaj Malarczyk <hcmiki358@gmail.com>
2023-08-22 11:07:24 +0200

    added site

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 //middleware
-app.use(cors({ origin: "https://getinhere.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://getinhere.pl", credentials: true }));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
