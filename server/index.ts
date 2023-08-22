43bd512e533a33c3e5680d57686d5f8321d9c914
Mikołaj Malarczyk <hcmiki358@gmail.com>
2023-08-22 12:13:37 +0200

    config cors

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 //middleware
-app.use(cors({ origin: "https://getinhere.pl", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
